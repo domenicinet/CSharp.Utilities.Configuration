02c400e34f35ce68ddc0bf12689a42ba6249739c
Alex Domenici <alex@2c2p.com>
2016-11-05 15:37:14 +0700

    Added sample solution.

---
+++ Samples/SampleApplication/Program.cs
@@ -0,0 +1,35 @@
+﻿using Domenici.Utilities.Configuration;
+using SampleDependency;
+using System;
+
+namespace SampleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dataManager = new DataManager();
+            Console.WriteLine(dataManager.GetStartMessage());
+            Console.ReadKey();
+            Console.WriteLine();
+
+            // Read all settings
+            string  applicationVersion = SampleApplicationSettings.ApplicationVersion;
+            string  stringSetting      = SampleApplicationSettings.DataSamples.StringSetting;
+            int     integerSetting     = SampleApplicationSettings.DataSamples.IntegerSetting;
+            decimal decimalSetting     = SampleApplicationSettings.DataSamples.DecimalSetting;
+            bool    booleanSetting     = SampleApplicationSettings.DataSamples.BooleanSetting;
+
+            Console.WriteLine($"App Version: {applicationVersion}");
+            Console.WriteLine($"String: {stringSetting}");
+            Console.WriteLine($"Integer: {integerSetting}");
+            Console.WriteLine($"Decimal: {decimalSetting}");
+            Console.WriteLine($"Boolean: {booleanSetting}");
+
+            // Done
+            Console.WriteLine();
+            Console.WriteLine(dataManager.GetEndMessage());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Samples/SampleDependency/DataManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Domenici.Utilities.Configuration;
+
+namespace SampleDependency
+{
+    public class DataManager
+    {
+        public string GetStartMessage()
+        {
+            return string.Format(SampleDependencySettings.SampleDependency.WelcomeMessage, SampleDependencySettings.SampleDependency.ApplicationName);
+        }
+
+        public string GetEndMessage()
+        {
+            return SampleDependencySettings.SampleDependency.DoneMessage;
+        }
+    }
+}
