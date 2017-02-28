2486ce1957c4d893c629923838c993ed544ace14
Alex Domenici <alex@2c2p.com>
2017-02-28 11:02:54 +0700

    Extended the unit test.

--- Sources/Domenici.Utilities.Configuration.Tests/TestSettingsManager.cs
+++ Sources/Domenici.Utilities.Configuration.Tests/TestSettingsManager.cs
@@ -9,10 +9,22 @@ namespace Domenici.Utilities.Configuration.Tests
         [TestMethod]
         public void TestSettingsManager()
         {
-            string test1 = SettingsManager.GetValue("TestSetting");
+            string test1 = SettingsManager.GetValue("TestSetting1");
             Assert.AreEqual("This is a test setting.", test1);
 
-            int test2 = (int)SettingsManager.GetValue("IntegerSetting", typeof(int));
+            int test2 = (int)SettingsManager.GetValue("IntegerSetting1", typeof(int));
+            Assert.AreEqual(100, test2);
+
+            test1 = SettingsManager.GetValue("TestSetting2");
+            Assert.AreEqual("This is a test setting.", test1);
+
+            test2 = (int)SettingsManager.GetValue("IntegerSetting2", typeof(int));
+            Assert.AreEqual(100, test2);
+
+            test1 = SettingsManager.GetValue("TestSetting3");
+            Assert.AreEqual("This is a test setting.", test1);
+
+            test2 = (int)SettingsManager.GetValue("IntegerSetting3", typeof(int));
             Assert.AreEqual(100, test2);
         }
     }
